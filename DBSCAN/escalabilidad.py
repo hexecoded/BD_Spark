2c7ef080445d429536c0e4d1b4849eb706afed90
Hexecode <cris190402@hotmail.com>
2025-04-25 23:02:07 +0200

    Clustering: version definitiva medicion

--- DBSCAN/escalabilidad.py
+++ DBSCAN/escalabilidad.py
@@ -1,12 +1,20 @@
+"""
+Escalabilidad de DBSCAN, grupo 4NN
+
+En este script, se pueden encontrar la  recopilación de datos para el 
+calculo del las métricas de escalado.
+
+Para ejecutar y obtener los csv, basta con descomentar las líneas que 
+rodean a los bucles for. Actualmente, si lo ejecuta comentado, se generarán
+sólo los gráficos a partir de los csv, en caso de que existan.
+"""
 import sys
 import pandas as pd
 from matplotlib import pyplot as plt
 import subprocess
 
-
 def plot_scalability_global(name, stats, xlabel, x_vals, linear_vals, ax):
-
-    ax.set_title(f"{name.capitalize()} of the DBSCAN implementation")
+    ax.set_title(f"{name.capitalize()} of DBSCAN")
     ax.plot(x_vals, stats[name], 'k', label=f"{name}")
     ax.plot(x_vals, linear_vals, '--', color='gray',
             label=f"Linear {name}", linewidth=1)
@@ -15,12 +23,9 @@ def plot_scalability_global(name, stats, xlabel, x_vals, linear_vals, ax):
 
     if name == 'scale-up':
         ax.set_ylim([0, 1.1])
-        
-    plt.show()
 
 
 def main(argv):
-
     # Create the plot
     fig, ax = plt.subplots(1, 3, figsize=(12, 4))
     fig.suptitle(f"Scalability of the DBSCAN (Grupo 4NN)")
@@ -42,6 +47,8 @@ def main(argv):
     avg_results = results.groupby("cores").mean()
     avg_results["speed-up"] = avg_results["runtime"][1] / \
         avg_results["runtime"]
+        
+    print(avg_results)
 
     plot_scalability_global('speed-up', avg_results,
                             "Number of cores", num_cores, num_cores, ax[0])
@@ -60,10 +67,10 @@ def main(argv):
         names=["cores", "percentage", "runtime"],
     )
     avg_results = results.groupby("percentage").mean()
-    avg_results["size-up"] = avg_results["runtime"] / \
-        avg_results["runtime"].loc[1.0]
-
+    avg_results["size-up"] = avg_results["runtime"] / avg_results["runtime"][0.1]
     linear_sizeup = [1, 2, 4, 8, 10]
+    print(avg_results)
+
     plot_scalability_global('size-up', avg_results,
                             "Size of data (%)", sizes, linear_sizeup, ax[1])
 
@@ -82,9 +89,9 @@ def main(argv):
         names=["cores", "percentage", "runtime"],
     )
     avg_results = results.groupby(["cores", "percentage"]).mean()
-    avg_results["scale-up"] = avg_results["runtime"][(
-        1, 1.0)] / avg_results["runtime"]
-
+    print(avg_results)
+    avg_results["scale-up"] = avg_results["runtime"][(1, 0.1)] / avg_results["runtime"]
+    print(avg_results)
     ideal_scaleup = [1]*5
     plot_scalability_global('scale-up', avg_results,
                             "Number of cores", num_cores, ideal_scaleup, ax[2])
@@ -94,8 +101,5 @@ def main(argv):
     plt.savefig(
         f"img/scalability_global.svg", bbox_inches="tight", pad_inches=0.0)
 
-
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-
